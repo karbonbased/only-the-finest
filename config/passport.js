5a02e4ff002185895a47365c61066981f5cc0750
Joshua Davis <karbonbased@gmail.com>
2016-03-06 16:01:15 -0500

    config passport.js file done

--- config/passport.js
+++ config/passport.js
@@ -2,12 +2,8 @@
 var User = require('../models/users.js');
 var LocalStrategy   = require('passport-local').Strategy;
 
-
-
-
-
 module.exports = function(passport) {
-
+console.log("passport file recognized")
 
 // ENCRYPT
 ////////////////////////////////////////////////
@@ -16,98 +12,95 @@ module.exports = function(passport) {
 // PASSPORT NEEDS TO SERIALIZE AND DESERIALIZE USERS FROM SESSION
 
   // USED TO SERIALIZE THE USER FOR THE SESSION
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-  // USED TO DESERIALIZE THE USER FOR THE SESSION
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
     });
-  });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
 
 
-// LOCAL SIGNUP 
-//////////////////////////////////////////////
-
-  passport.use('local-signup', new LocalStrategy({
-    // CHANGING THE USERNAME FIELD TO EMAIL
-    usernameField: 'email',
-    passwordField: 'password',
-    // ALLOWS THE REQUEST TO BE PASSED AS CALLBACK
-    passReqToCallback: true
-
-    }, 
-    function(req, email, password, done) {
-  
-    // ASYNCHRONOUS, WON'T HAPPEN
-    process.nextTick(function() {
-    // FIND THE USER WITH THAT EMAIL
-    User.findOne({ 'email': email }, function(err, user) {
-
-      // RETURN ANY ERROR
-      if (err) { return done(err) }
-
-      // DOES THE USER EXIST? 
-      if (user) { 
-
-        // IF THEY DO........
-        return done(null, false); 
-
-        // IF THEY DON'T SET THEM UP
-      } else {
-        var newUser = new User();
-        newUser.email = email;
-        newUser.password = newUser.generateHash(password);
-        newUser.username = req.body.username;
-
-        // SAVE THE NEW USER
-        newUser.save(function(err) {
-          if (err) { 
-          //console.log(err)
-            throw err;
-            return done(null, newUser);
-          }   // END IF
-        });   // END USER SAVE
-      }       // END USER EXISTS
-    })        // END FIND USER
-    })        // END NEXT TICK
-    }         // END LOCAL STRATEGY
-  ));         // END PASSPORT LOCAL-SIGNUP
-
-
-// LOCAL LOGIN
-//////////////////////////////////////////////
-
-  passport.use('local-login', new LocalStrategy({
-    // CHANGING THE USERNAME FIELD TO EMAIL
-    usernameField: 'email',
-    passwordField: 'password',
-    // PASS THE REQUEST TO THE CALLBACK
-    passReqToCallback: true
-    }, 
-    // CALLBACK WITH THE EMAIL AND PASSWORD FROM FORM
+    // SET UP THE LOCAL STRATEGY FOR SIGN-UP
+passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
     function(req, email, password, done) {
 
-    // FIND A USER WITH THE FORM EMAIL  
-    User.findOne({ 'email': email }, function(err, user) {
+        // asynchronous
+        // User.findOne wont fire unless data is sent back
+        process.nextTick(function() {
 
-      // RETURN ANY ERRORS
-      if (err) { return done(err) }
-      // IF NO USER IS FOUND  
-      if (!user) {
-        console.log('NO USER FOUND');
-        return done(null, false);
-      }
-      // IF THE USER IS FOUND BUT THE EMAIL IS WRONG
-      if (!user.validPassword(password)) {
-        return done(null, false);
-      }
-      // IF IT'S SUCCESSFUL
-      return done(null, user);
-    });       // END FIND USER
-  }           // END LOCAL STRATEGY
-  ));         // END PASSPORT LOGIN
-}             // END MODULE.EXPORTS
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'email' :  email }, function(err, user) {
+            // if there are any errors, return the error
+            if (err)
+                return done(err);
 
+            // check to see if theres already a user with that email
+            if (user) {
+                return done(null, false);
+            } else {
 
+                // if there is no user with that email
+                // create the user
+                var newUser            = new User();
+
+                // set the user's local credentials
+                newUser.name = req.body.name;
+                newUser.gender = req.body.gender;
+                newUser.email    = email;
+                newUser.password = newUser.hash(password);
+
+                // save the user
+                newUser.save(function(err) {
+                    if (err)
+                        throw err;
+                    return done(null, newUser);
+                });
+            }
+
+        });    
+
+        });
+
+    }));
+
+// SIGN IN LOCAL STRATEGY
+ passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our form
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'email' :  email }, function(err, user) {
+            // if there are any errors, return the error before anything else
+            if (err)
+                return done(err);
+
+            // if no user is found, return the message
+            if (!user)
+                return done(null, false, console.log('No user found')); 
+
+            // if the user is found but the password is wrong
+            if (!user.validPassword(password))
+                return done(null, false, console.log('Wrong password.'));
+
+            // all is well, return successful user
+            return done(null, user);
+        });
+
+    }));
+
+} // close module exports
