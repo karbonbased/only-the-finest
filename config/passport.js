271280bc9662925d85eebca2a1012d8951b8e91e
Joshua Davis <karbonbased@gmail.com>
2016-03-06 18:40:19 -0500

    congfiguring passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 //var passport = require('passport');
-var User = require('../models/users.js');
+var User = require('../models/user.js');
 var LocalStrategy   = require('passport-local').Strategy;
 
 module.exports = function(passport) {
