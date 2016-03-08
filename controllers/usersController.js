46d9c5c47b3280fe23b26a196502e308b0744715
EMBOT415 <embot415@gmail.com>
2016-03-07 20:08:35 -0500

    updating locations

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -18,9 +18,45 @@ router.get('/', function(req, res){
 // SEED ROUTE FOR USERS
 router.get('/seed', function(req, res){
 	var users = [
-		{name: 'Jen', email: 'Jen@Jen.com' , gender: 'f', password: 'Jen'},
-		{name: 'Emily', email: 'Emily@Emily.com' , gender: 'f', password: 'Emily'},
-		{name: 'Josh', email: 'Josh@Josh.com' , gender: 'm', password: 'Josh'},
+		{name: 'Jen', email: 'Jen@Jen.com' , gender: 'f', password: 'Jen', locations: 	
+			[{name: 'Estela',
+			lat: 40.724777, 
+			lng: -73.994658},
+
+			{name: 'ABC Kitchen',
+			lat: 40.738237,
+			lng: -73.991126},
+
+			{name: 'Buvette',
+			lat: 40.729331,
+			lng:  -73.983839}
+			],
+			{name: 'Emily', email: 'Emily@Emily.com' , gender: 'f', password: 'Emily', locations: 	
+			[{name: 'Blue Bottle Coffee',
+			lat: 40.729331, 
+			lng: -73.983839},
+
+			{name: 'Cafe Mogador',
+			lat: 40.721577,
+			lng: -73.959653},
+
+			{name: 'Bird',
+			lat: 40.714248,
+			lng:  -73.960202}
+			}],
+			{name: 'Josh', email: 'Josh@Josh.com' , gender: 'm', password: 'Josh', locations: 	
+			[{name: 'Liquiteria',
+			lat: 40.738237, 
+			lng:  -73.991126},
+
+			{name: 'Apple',
+			lat: 40.752936,
+			lng: -73.976896},
+
+			{name: 'ECO Pets NYC',
+			lat: 40.719895,
+			lng:  -73.963611}
+			}],
 	];
 
 	// CREATE THE SEEDED USERS IN DB
